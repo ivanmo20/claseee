270f8a79eae750e807e8ffdf945512dba428f443
ivanmo20 <63992740+ivanmo20@users.noreply.github.com>
2020-07-19 22:59:03 -0500

    creando

---
+++ src/main/java/pe/pucp/sw2/claseee/ClaseeeApplication.java
@@ -0,0 +1,13 @@
+package pe.pucp.sw2.claseee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaseeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaseeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/pucp/sw2/claseee/ClaseeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.pucp.sw2.claseee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaseeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
